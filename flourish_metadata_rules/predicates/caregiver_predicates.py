9a3752b4b344932c0d3e070ca98ed89eedef7f88
tumiemosweu <imosweuk@gmail.com>
2021-03-18 10:27:38 +0200

    update cohort reference

--- flourish_metadata_rules/predicates/caregiver_predicates.py
+++ flourish_metadata_rules/predicates/caregiver_predicates.py
@@ -1,7 +1,7 @@
 from django.apps import apps as django_apps
 from edc_constants.constants import POS, IND, YES
 from edc_metadata_rules import PredicateCollection
-from flourish_caregiver.helper_classes import Cohort
+
 
 class CaregiverPredicates(PredicateCollection):
 
@@ -13,14 +13,16 @@ class CaregiverPredicates(PredicateCollection):
 
         rapid_test_model = django_apps.get_model(f'{self.app_label}.hivrapidtestcounseling')
         try:
-            rapid_test_obj = rapid_test_model.objects.get(maternal_visit__subject_identifier=visit.subject_identifier)
+            rapid_test_obj = rapid_test_model.objects.get(
+                maternal_visit__subject_identifier=visit.subject_identifier)
         except rapid_test_model.DoesNotExist:
 
             prior_participation = self.prior_participation(visit=visit)
             if prior_participation:
                 return prior_participation.mom_hivstatus
             else:
-                enrollment_model = django_apps.get_model(f'{self.app_label}.antenatalenrollment')
+                enrollment_model = django_apps.get_model(
+                    f'{self.app_label}.antenatalenrollment')
                 try:
                     enrollment_obj = enrollment_model.objects.get(
                                                 subject_identifier=visit.subject_identifier)
@@ -42,8 +44,7 @@ class CaregiverPredicates(PredicateCollection):
         else:
             maternal_delivery_cls = django_apps.get_model(f'{self.app_label}.maternaldelivery')
             try:
-                maternal_delivery_obj = maternal_delivery_cls.objects.get(
-                                        subject_identifier=visit.subject_identifier)
+                maternal_delivery_cls.objects.get(subject_identifier=visit.subject_identifier)
             except maternal_delivery_cls.DoesNotExist:
                 return True
         return False
@@ -59,7 +60,7 @@ class CaregiverPredicates(PredicateCollection):
             return True
 
     def func_preg_no_prior_participation(self, visit=None, **kwargs):
-        """Returns true if participant is expecting and never 
+        """Returns true if participant is expecting and never
         participated in a BHP study for enrollment_visit.
         """
         return self.pregnant(visit=visit) and not self.prior_participation(visit=visit)
@@ -78,15 +79,17 @@ class CaregiverPredicates(PredicateCollection):
             cyhuu_model = django_apps.get_model(f'{self.pre_app_label}.cyhuupreenrollment')
 
             try:
-                cyhuu_obj = cyhuu_model.objects.get(maternal_visit__appointment__subject_identifier=visit.subject_identifier)
+                cyhuu_obj = cyhuu_model.objects.get(
+                    maternal_visit__appointment__subject_identifier=visit.subject_identifier)
             except cyhuu_model.DoesNotExist:
                 return False
             else:
-                return cyhuu_obj.biological_mother == YES and self.hiv_status(visit=visit) == POS
+                return (cyhuu_obj.biological_mother == YES
+                        and self.hiv_status(visit=visit) == POS)
 
     def func_pregnant_hiv(self, visit=None, **kwargs):
         """Returns true if a newly enrolled participant is pregnant and living with HIV.
-        """ 
+        """
         return self.pregnant(visit=visit) and self.hiv_status(visit=visit) == POS
 
     def func_non_pregnant_caregivers(self, visit=None, **kwargs):
