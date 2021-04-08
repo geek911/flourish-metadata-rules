6c560e0bed0fca8471d4a03c15ad9f0b8d0f0b67
ame diphoko <diphokoame@gmail.com>
2021-04-08 12:45:38 +0200

    add rules forr depression screening forms

---
+++ flourish_metadata_rules/metadata_rules/caregiver_rule_groups/phq_depression_screening_rules.py
@@ -0,0 +1,19 @@
+from edc_metadata import NOT_REQUIRED, REQUIRED
+from edc_metadata_rules import CrfRule, CrfRuleGroup, register, PF
+
+app_label = 'flourish_caregiver'
+
+
+@register()
+class PHQ9DeprScreeningRuleGroup(CrfRuleGroup):
+
+    phq_screening_referral = CrfRule(
+        predicate=PF('depression_score', 'self_harm',
+                     func=lambda score, self_harm: True if score >= 5 or self_harm != '0' else False),
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_models=[f'{app_label}.caregiverphqreferral'])
+
+    class Meta:
+        app_label = app_label
+        source_model = f'{app_label}.caregiverphqdeprscreening'
