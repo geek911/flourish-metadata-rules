9f72022c1ecb4260462a88ac111a7a42d8f0d501
McTurner45 <58169969+nmunatsi@users.noreply.github.com>
2022-05-31 16:27:44 +0200

    tb enrolment visit rules

---
+++ flourish_metadata_rules/metadata_rules/caregiver_rule_groups/ultrasound_rules.py
@@ -0,0 +1,21 @@
+from edc_metadata import NOT_REQUIRED, REQUIRED
+from edc_metadata_rules import CrfRule, CrfRuleGroup, register
+
+from flourish_metadata_rules.predicates import CaregiverPredicates
+
+app_label = 'flourish_caregiver'
+pc = CaregiverPredicates()
+
+
+@register()
+class UltrasoundRuleGroup(CrfRuleGroup):
+    tb_eligible = CrfRule(
+        predicate=pc.tb_eligible,
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_models=[f'{app_label}.tbstudyeligibility', ])
+
+
+    class Meta:
+        app_label = app_label
+        source_model = f'{app_label}.ultrasound'
