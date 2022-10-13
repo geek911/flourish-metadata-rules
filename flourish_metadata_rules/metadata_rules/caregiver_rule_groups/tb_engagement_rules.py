7f024ad5d6b936fe36bb295774e9dd5e0fe809f0
Itumeleng Mosweu <imosweuk@gmail.com>
2022-10-13 16:36:40 +0200

    minor fix

--- flourish_metadata_rules/metadata_rules/caregiver_rule_groups/tb_engagement_rules.py
+++ flourish_metadata_rules/metadata_rules/caregiver_rule_groups/tb_engagement_rules.py
@@ -1,3 +1,4 @@
+from edc_constants.constants import YES
 from edc_metadata import NOT_REQUIRED, REQUIRED
 from edc_metadata_rules import CrfRule, CrfRuleGroup, register, P
 
@@ -8,7 +9,7 @@ app_label = 'flourish_caregiver'
 class TbEngagementRuleGroup(CrfRuleGroup):
 
     tbinterview = CrfRule(
-        predicate=P('interview_consent', 'eq', 'YES'),
+        predicate=P('interview_consent', 'eq', YES),
         consequence=REQUIRED,
         alternative=NOT_REQUIRED,
         target_models=[f'{app_label}.tbinterview'])
