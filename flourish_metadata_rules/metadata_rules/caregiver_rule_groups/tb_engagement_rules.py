0250bbdb076018f6a6c1f6b82b38f45dca74c344
Itumeleng Mosweu <imosweuk@gmail.com>
2022-10-13 13:47:47 +0200

    add tb engagement rule group

---
+++ flourish_metadata_rules/metadata_rules/caregiver_rule_groups/tb_engagement_rules.py
@@ -0,0 +1,18 @@
+from edc_metadata import NOT_REQUIRED, REQUIRED
+from edc_metadata_rules import CrfRule, CrfRuleGroup, register, P
+
+app_label = 'flourish_caregiver'
+
+
+@register()
+class TbEngagementRuleGroup(CrfRuleGroup):
+
+    tbinterview = CrfRule(
+        predicate=P('interview_consent', 'eq', 'YES'),
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_models=[f'{app_label}.tbinterview'])
+
+    class Meta:
+        app_label = app_label
+        source_model = f'{app_label}.tbengagement'
