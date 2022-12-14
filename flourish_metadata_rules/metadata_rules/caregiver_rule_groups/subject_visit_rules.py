cefd4942919f51189aa036dd0e744b13e967f391
salahdin <salahdinjemal@gmail.com>
2022-12-14 09:45:09 +0200

    replace tbroutine with version 2

--- flourish_metadata_rules/metadata_rules/caregiver_rule_groups/subject_visit_rules.py
+++ flourish_metadata_rules/metadata_rules/caregiver_rule_groups/subject_visit_rules.py
@@ -20,7 +20,7 @@ class MaternalVisitRuleGroup(CrfRuleGroup):
                        f'{app_label}.tbscreenpreg',
                        f'{app_label}.tbpresencehouseholdmembers',
                        f'{app_label}.substanceusepriorpregnancy',
-                       f'{app_label}.tbroutinehealthscreen', ])
+                       f'{app_label}.tbroutinehealthscreenv2', ])
 
     biological_with_hiv_not_preg = CrfRule(
         predicate=pc.func_positive_prior_participant,
