e598e90e7cf49b4b027737d681c6197a1461a5f5
Moses Chawawa <moseschawawa@gmail.com>
2023-05-05 10:46:50 +0200

    Added rules for req and a crf

--- flourish_metadata_rules/metadata_rules/child_rule_groups/requisition_rules/child_visit_req_rules.py
+++ flourish_metadata_rules/metadata_rules/child_rule_groups/requisition_rules/child_visit_req_rules.py
@@ -2,7 +2,7 @@ from edc_metadata import NOT_REQUIRED, REQUIRED
 from edc_metadata_rules import RequisitionRule, RequisitionRuleGroup, register
 
 from flourish_labs import dna_pcr_panel, stool_sample_panel, infant_pl_cytokines_panel
-from flourish_labs import rectal_swab_panel
+from flourish_labs import rectal_swab_panel, lithium_heparin_panel
 from ....predicates import ChildPredicates
 
 app_label = 'flourish_child'
@@ -32,6 +32,12 @@ class ChildVisitReqRuleGroup(RequisitionRuleGroup):
         alternative=NOT_REQUIRED,
         target_panels=[infant_pl_cytokines_panel,
                        rectal_swab_panel])
+
+    lithium_heparin_panel_rule = RequisitionRule(
+        predicate=pc.func_tb_lab_results_exist,
+        consequence=REQUIRED,
+        alternative=NOT_REQUIRED,
+        target_panels=[lithium_heparin_panel,])
 
     class Meta:
         app_label = app_label
