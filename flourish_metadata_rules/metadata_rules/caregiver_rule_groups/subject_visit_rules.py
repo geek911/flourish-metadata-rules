303c9786226e2ee492034bd98f35822c73f503a0
Moses Chawawa <moseschawawa@gmail.com>
2023-07-19 13:57:31 +0200

    FIX: Removed duplication

--- flourish_metadata_rules/metadata_rules/caregiver_rule_groups/subject_visit_rules.py
+++ flourish_metadata_rules/metadata_rules/caregiver_rule_groups/subject_visit_rules.py
@@ -74,8 +74,7 @@ class MaternalVisitRuleGroup(CrfRuleGroup):
         predicate=pc.func_show_hiv_test_form,
         consequence=REQUIRED,
         alternative=NOT_REQUIRED,
-        target_models=[f'{app_label}.hivrapidtestcounseling', 
-                       f'{app_label}.posthivrapidtestandconseling'])
+        target_models=[f'{app_label}.hivrapidtestcounseling',])
     
     post_hiv_test = CrfRule(
         predicate=pc.func_post_hiv_rapid_test,
